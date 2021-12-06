8db91669f6244e239f0cceaae78c120e3eb929ff
Artem Shibaev <artem.shibaev@streamlabs.ru>
2021-12-06 14:45:35 +0300

    Fix dynamic script

--- yolo_to_trt_dynamic_batch.sh
+++ yolo_to_trt_dynamic_batch.sh
@@ -7,6 +7,6 @@ NUM_CLASSES=$4
 BATCH_SIZE=$5
 CONTAINER_VER=$6
 
-cd /tensorrt_demos/plugins/batch && rm -f *.so *.o && make && cp libyolo_layer.so ../libyolo_layer.so && cd /tensorrt_demos
+cd /tensorrt_demos/plugins/dynamic && rm -f *.so *.o && make && cp libyolo_layer.so ../libyolo_layer.so && cd /tensorrt_demos
 python3 yolo/yolo_to_onnx.py -m $MODEL
 python3 yolo/onnx_to_tensorrt_dynamic_batch.py -m $MODEL -b $BATCH_SIZE -v -c $NUM_CLASSES --width $WIDTH --height $HEIGHT --ver $CONTAINER_VER
